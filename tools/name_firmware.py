84b63a88adfb2c5d2b7255dbcdaa74d07b16da03
lbussy <lee@bussy.org>
2019-08-29 14:17:26 -0500

    Update for current project

--- tools/name_firmware.py
+++ tools/name_firmware.py
@@ -23,20 +23,8 @@ Import("env")
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 
-# Parse out defined shield from Config.h
-shields = []
-linenum = 0
-define = "#define BREWPI_STATIC_CONFIG".lower()
-with open (str(env["PROJECTINCLUDE_DIR"]) + '/Config.h', 'rt') as config:
-    for line in config:
-        linenum += 1
-        if line.lower().startswith(define):
-            shields.append(line.rstrip('\n'))
-for variant in shields:
-    shield = variant.split(" ")[-1].split("_")[-1].lower()
-
-env.Replace(PROGNAME="brewpi-%s-%s-%s-%s" % (
-    env["PIOFRAMEWORK"][0],
+env.Replace(PROGNAME="brewbubbles-%s-%s-%s-%s" % (
     str(env["BOARD"]),
-    shield,
-    defines.get("PIO_SRC_TAG")))
+    defines.get("PIO_SRC_TAG"),
+    defines.get("PIO_SRC_REV"),
+    defines.get("PIO_SRC_BRH")))
