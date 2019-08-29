e18451a4b21e766bd77bb34cd02de498fae5d074
lbussy <lee@bussy.org>
2019-08-29 14:21:20 -0500

    Add a debug delay

--- src/serial.cpp
+++ src/serial.cpp
@@ -20,6 +20,7 @@ with Brew Bubbles. If not, see <https://www.gnu.org/licenses/>. */
 #ifndef DISABLE_LOGGING
 
 void serial() { // Start serial with auto-detected rate (default to BAUD)
+    _delay(4000); // DEBUG TODO
     Serial.begin(BAUD);
     Serial.setDebugOutput(true);
     Serial.flush();
