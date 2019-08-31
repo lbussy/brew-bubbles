6c5b0a054cb1662cdcb6b7a1d0e129155e8196ee
Lee Bussy <lee@bussy.org>
2019-08-31 13:41:49 -0500

    Create choice for connecting wifi

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -17,11 +17,50 @@ with Brew Bubbles. If not, see <https://www.gnu.org/licenses/>. */
 
 #include "wifi.h"
 
-void presentPortal() { // Present AP and captive portal to allow new settings
+bool shouldSaveConfig = false;
+
+void presentPortal(bool ignore = false) { // Present AP and captive portal to allow new settings
     JsonConfig *config = JsonConfig::getInstance();
+
     WiFiManager wifiManager;
-    wifiManager.setConfigPortalTimeout(120); // Reset controller after 120 secs
-    wifiManager.startConfigPortal(config->ssid, config->appwd);
+    wifiManager.setSaveConfigCallback(saveConfigCallback); // Set callback to save settings
+    wifiManager.setAPCallback(configModeCallback); // Give some basic instructions via Serial
+
+    //wifiManager.resetSettings(); // DEBUG
+
+    wifi_station_set_hostname(config->hostname);
+
+    if (ignore) {
+        wifiManager.setTimeout(120);
+        if (wifiManager.startConfigPortal(config->ssid, config->appwd)) {
+            if (shouldSaveConfig) { // Save configuration
+                config->Save();
+            }
+        } else {
+            // Hit timeout on voluntary portal
+        }
+    } else { // Normal WiFi connection attempt
+        if (!wifiManager.autoConnect(config->ssid, config->appwd)) {
+            Log.warning(F("Failed to connect and hit timeout."));
+            _delay(3000);
+            ESP.reset();
+            _delay(5000);
+        }
+    }
+
+    wifi_station_set_hostname(config->hostname);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        _delay(500);
+        #ifdef LOG_LEVEL
+        Serial.print(F("."));
+        #endif
+    }
+    #ifdef LOG_LEVEL
+    Serial.println();
+    #endif
+    Log.notice(F("Connected. IP address: %s." CR), WiFi.localIP().toString().c_str());
 }
 
 void resetWifi() { // Wipe wifi settings and reset controller
@@ -29,4 +68,15 @@ void resetWifi() { // Wipe wifi settings and reset controller
     wifiManager.resetSettings();
     _delay(3000);
     ESP.reset();
+}
+
+void saveConfigCallback() { // Set flag to save config
+    Log.verbose(F("Callback: Saving config."));
+    shouldSaveConfig = true;
+}
+
+void configModeCallback(WiFiManager *myWiFiManager) {
+    Log.notice(F("Entered portal mode; name: %s, IP: %s." CR),
+        myWiFiManager->getConfigPortalSSID().c_str(),
+        WiFi.localIP().toString().c_str());
 }
