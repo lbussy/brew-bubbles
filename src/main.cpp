15d35f9245d78a6989631ed6f4121e909ead6d51
Lee Bussy <lee@bussy.org>
2019-08-31 13:42:56 -0500

    Cleanup unused items

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,9 @@ with Brew Bubbles. If not, see <https://www.gnu.org/licenses/>. */
 #include "main.h"
 
 WebServer *server = WebServer::getInstance();
-Bubbles *bubble = Bubbles::getInstance();
-NtpHandler *ntpTime = NtpHandler::getInstance();
-
 DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
 
-void setup(void)
-{
+void setup(void) {
     bool rst = drd.detect(); // Check for double-reset
     serial();
 
@@ -32,56 +28,26 @@ void setup(void)
     pinMode(RESETWIFI, INPUT_PULLUP);
     if (digitalRead(RESETWIFI) == LOW) {
         Log.notice(F("%s low, presenting portal." CR), RESETWIFI);
-        presentPortal();
+        presentPortal(true);
     } else if (rst == true) {
         Log.notice(F("DRD: Double reset boot, presenting portal." CR));
-        presentPortal();
+        presentPortal(true);
     } else {
         Log.verbose(F("DRD: Normal boot, re-using WiFi values." CR));
-        JsonConfig *config = JsonConfig::getInstance();
-        wifi_station_set_hostname(config->hostname);
-
-        Log.notice(F("Connecting to %s." CR), WiFi.SSID().c_str());
-        if (String(WiFi.SSID()) != String(config->ssid)) {
-            WiFi.mode(WIFI_STA);
-            WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-        }
-
-        while (WiFi.status() != WL_CONNECTED)
-        {
-            _delay(500);
-            #ifdef LOG_LEVEL
-            Serial.print(F("."));
-            #endif
-        }
-        #ifdef LOG_LEVEL
-        Serial.println();
-        #endif
-
-        ntpTime->start();
-        ntpTime->update();
-        while (!ntpTime->hasBeenSet) {
-            #ifdef LOG_LEVEL
-            Serial.print(F("."));
-            #endif
-            _delay(500);
-            ntpTime->update();
-            yield();
-        }
-        #ifdef LOG_LEVEL
-        Serial.println();
-        Log.notice(F("NTP Time: %s." CR), ntpTime->getJsonTime());
-        #endif
-
-        if (!MDNS.begin(config->hostname)) {
-            Log.error(F("Error setting up MDNS responder."));
-        } else {
-            Log.notice(F("mDNS responder started, hostname %s.local." CR), WiFi.hostname().c_str());
-        }
-        server->initialize(PORT);
-
-        Log.notice(F("Connected. IP address: %s." CR), WiFi.localIP().toString().c_str());
+        presentPortal(false);
     }
+
+    JsonConfig *config = JsonConfig::getInstance();
+    if (!MDNS.begin(config->hostname)) {
+        Log.error(F("Error setting up MDNS responder."));
+    } else {
+        Log.notice(F("mDNS responder started, hostname %s.local." CR), WiFi.hostname().c_str());
+    }
+
+    server->initialize(PORT); // Turn on web server
+
+    NtpHandler *ntpTime = NtpHandler::getInstance();
+    ntpTime->start();
 
     const char* version = stringify(PIO_SRC_TAG);
     const char* build = stringify(PIO_SRC_REV);
@@ -90,9 +56,11 @@ void setup(void)
     Log.notice("Started Brew Bubbles version %s (%s) [%s]." CR, version, branch, build);
 }
 
-void loop(void)
-{
-    bubble->Update();
-    server->handleLoop();
-    MDNS.update();
+void loop(void) {
+    Bubbles *bubble = Bubbles::getInstance();
+    while (true) {
+        bubble->Update();
+        server->handleLoop();
+        MDNS.update();
+    }
 }
