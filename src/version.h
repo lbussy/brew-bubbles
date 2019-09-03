3a408e6aad897260e16896541c0ca6c1060deb70
Lee Bussy <lee@bussy.org>
2019-09-03 07:47:49 -0500

    Add versioning capabilities

---
+++ src/version.h
@@ -0,0 +1,32 @@
+/* Copyright (C) 2019 Lee C. Bussy (@LBussy)
+
+This file is part of Lee Bussy's Brew Bubbles (Brew-Bubbles).
+
+Brew Bubbles is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+Brew Bubbles is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Brew Bubbles. If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#include <Arduino.h>
+
+#define stringify(s) _stringifyDo(s)
+#define _stringifyDo(s) #s
+
+const char* build();
+const char* branch();
+const char* version();
+
+int versionCompare(String, String);
+
+#endif // _VERSION_H
