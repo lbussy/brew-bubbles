bfa38b7d1dc309fa573b10b8607170a5aac607e4
Lee Bussy <lee@bussy.org>
2019-10-01 12:28:46 -0500

    Build out additional pages

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import recommonmark
+from recommonmark.transform import AutoStructify
 
 
 # -- Project information -----------------------------------------------------
@@ -170,3 +172,11 @@ epub_title = project
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
+# USe AutoStructify tp convert markdown to rST
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
