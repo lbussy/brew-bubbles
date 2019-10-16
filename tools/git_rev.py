73629e1828d564ac7e51f2817e1627bf0f53174e
Lee Bussy <lee@bussy.org>
2019-10-16 18:54:32 -0400

    Add project name to firmware naming

--- tools/git_rev.py
+++ tools/git_rev.py
@@ -19,6 +19,12 @@
 
 import subprocess
 
+# Get Git project name
+projcmd = "git rev-parse --show-toplevel"
+project = subprocess.check_output(projcmd, shell=True).decode().strip()
+project = project.split("/")
+project = project[len(project)-1]
+
 # Get 0.0.0 version from latest Git tag
 tagcmd = "git describe --tags --abbrev=0"
 version = subprocess.check_output(tagcmd, shell=True).decode().strip()
@@ -32,6 +38,7 @@ branchcmd = "git rev-parse --abbrev-ref HEAD"
 branch = subprocess.check_output(branchcmd, shell=True).decode().strip()
 
 # Make all available for use in the macros
+print("-DPIO_SRC_NAM={0}".format(project))
 print("-DPIO_SRC_TAG={0}".format(version))
 print("-DPIO_SRC_REV={0}".format(commit))
 print("-DPIO_SRC_BRH={0}".format(branch))
