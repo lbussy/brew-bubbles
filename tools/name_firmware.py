8276277db985cd2da58c26aeaeb7e58018da5ca2
Lee Bussy <lee@bussy.org>
2019-10-16 18:54:32 -0400

    Add project name to firmware naming

--- tools/name_firmware.py
+++ tools/name_firmware.py
@@ -23,7 +23,8 @@ Import("env")
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 
-env.Replace(PROGNAME="brewbubbles-%s-%s-%s-%s" % (
+env.Replace(PROGNAME="%s-%s-%s-%s-%s" % (
+    defines.get("PIO_SRC_NAM"),
     str(env["BOARD"]),
     defines.get("PIO_SRC_TAG"),
     defines.get("PIO_SRC_REV"),
