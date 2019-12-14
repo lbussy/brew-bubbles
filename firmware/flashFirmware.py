a2391a6f45f91549c1cb9b29c077ba067a5ddcc0
Lee Bussy <lee@bussy.org>
2019-12-14 16:59:42 -0600

    Initial installer

--- firmware/flashFirmware.py
+++ firmware/flashFirmware.py
@@ -17,50 +17,136 @@
 # You should have received a copy of the GNU General Public License along
 # with Brew Bubbles. If not, see <https://www.gnu.org/licenses/>.
 
-import subprocess
-import os
-import sys
-import time
-
-exe = 'esptool.py'          # esptool.py v2.7 - ESP8266 ROM Bootloader Utility
-chip = 'auto'               # {auto,esp8266,esp32}
+exe = 'esptool.py'          # esptool.py v2.7
+chip = 'esp8266'            # {auto,esp8266,esp32}
 before = 'default_reset'    # {hard_reset,soft_reset,no_reset}
 after = 'hard_reset'        # {hard_reset,soft_reset,no_reset}
 arg = 'write_flash'         # Execute
 
-img1 = 'firmware.bin'
-img2 = 'spiffs.bin'
-loc1 = '0x00000000'
-loc2 = '0x00300000'
+img1 = 'firmware.bin'       # Firmware name
+loc1 = '0x00000000'         # Address for Firmware
+img2 = 'spiffs.bin'         # SPIFFS name
+loc2 = '0x00300000'         # Address for SPIFFs
 
-cmd = exe + ' --chip ' + chip + ' --before ' + before + ' --after ' + \
-    after + ' ' + arg + ' ' + loc1 + ' ' + img1 + ' ' + loc2 + ' ' + img2
+logoname = 'logo.gif'               # Logo for GUI
+appName = 'Controller Flash Tool'   # Used for title bar
 
-try:
-    FNULL = open(os.devnull, 'w')
-    x = subprocess.check_call(exe + ' -h', shell=True, stdout=FNULL, stderr=FNULL)
-except subprocess.CalledProcessError:
-    print('\nError:  ' + exe + ' not found.')
-    sys.exit(-1)
-except:
-    print('Unknown error detected.')
-    sys.exit(-1)
-else:
-    try:
-        print('\nFlashing controller.  This may take a minute.')
-        env = os.environ
-        subprocess.check_output(cmd, shell=True) # ), stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    except subprocess.CalledProcessError:
-        print('\nError:  ' + exe + ' returned a process error.')
-        sys.exit(-1)        
-    except:
-        print('\nUnknown error detected while flashing controller.')
-        print('Waiting to exit at except.')
-        sys.exit(-1)
+import subprocess
+import os
+import sys
+import time
+from appJar import gui
+import shlex
+from subprocess import Popen, PIPE
+import shutil
+import threading
+
+path = None
+app = None
+running = False
+
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+
+def getPath():
+    ospath = os.path.dirname(os.path.realpath(__file__))
+    if getattr(sys, 'frozen', False):
+        path = sys._MEIPASS
     else:
-        print('\nController flash completed successfully.')
+        path = ospath
+    return path
 
-print('Waiting to exit.')
-time.sleep(10)
+def press(button):
+    global running
+    if not running:
+        if button == "Continue":
+            t = threading.Thread(target=handleFlash)
+            t.start()
+        else:
+            pass
 
-sys.exit(0)
+def createGui(path):
+    global app
+    if getattr(sys, 'frozen', False):
+        logo = "{0}\{1}\{2}".format(path, logoname, logoname)
+    else:
+        logo = "{0}\{1}".format(path, logoname)
+    # Overall attributes
+    app.setBg("white")
+    app.setFont(18)
+    # Add Image
+    app.startLabelFrame("", 0, 0)
+    app.addImage("", logo)
+    app.stopLabelFrame()
+    # Add & configure widgets
+    app.addLabel("title", "Press 'Continue' to flash firmware")
+    app.setLabelBg("title", "orange")
+    # Link the buttons to the function called press
+    app.addNamedButton("Continue", "Continue", press)
+    # Start the GUI
+    app.go()
+
+def handleFlash():
+    global running
+    global path
+    running = True
+
+    # Use unbuffered
+    u = Unbuffered
+    sys.stderr = u(sys.__stderr__)
+    sys.stdout = u(sys.__stdout__)
+
+    if getattr(sys, 'frozen', False):
+        firmware = "{0}\{1}\{2}".format(path, img1, img1)
+        spiffs = "{0}\{1}\{2}".format(path, img2, img2)
+    else:
+        firmware = "{0}\{1}".format(path, img1)
+        spiffs = "{0}\{1}".format(path, img2)
+
+    # esptool.py --chip esp8266 --before default_reset --after hard_reset write_flash 0x00000000 firmware.bin 0x00300000 spiffs.bin
+    process = subprocess.Popen([
+            shutil.which('esptool.py'),
+            '--chip', chip,
+            '--before', before,
+            '--after', after,
+            arg,
+            loc1, firmware,
+            loc2, spiffs
+        ],
+        stdout=subprocess.PIPE,
+        universal_newlines=True)
+
+    while True: # Loop while running
+        output = process.stdout.readline()
+        print(output.strip())
+        # TODO:  Maybe status updates according to output?
+        return_code = process.poll()
+        if return_code is not None:
+            print('Return code = {0}'.format(return_code))
+            # Process has finished, read rest of the output 
+            for output in process.stdout.readlines():
+                print(output.strip())
+            break
+
+def main():
+    global app
+    global path
+    global appName
+    path = getPath()
+    app = gui(appName)
+    createGui(path)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+    sys.exit(0)
