c83f3f11884c355966d9ca802fec6e20d863af2b
Lee Bussy <lee@bussy.org>
2020-03-22 17:46:16 -0500

    Add async thatVersion lookup

---
+++ src/thatVersion.cpp
@@ -0,0 +1,99 @@
+/* Copyright (C) 2019-2020 Lee C. Bussy (@LBussy)
+
+This file is part of Lee Bussy's Brew Bubbles (brew-bubbles).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+#include "thatVersion.h"
+
+asyncHTTPrequest request;
+ThatVersion __attribute__((unused)) thatVersion;
+
+void sendRequest() {
+    if (request.readyState() == 0 || request.readyState() == 4) {
+        request.open("GET", VERSIONJSONLOC);
+        request.send();
+    }
+}
+
+void requestHandler(void* optParm, asyncHTTPrequest* request, int readyState) {
+    String body = request->responseText();
+    const char * src = body.c_str();
+    TV_NOT(src);
+    if (!deserializeVersion(src, thatVersion)) {
+        TV_ERR(F("failed to deserialize version"));
+    } else {
+        TV_NOT(F("version deserialized"));
+    }
+}
+
+bool serializeVersion(const ThatVersion &thatVersion, Print &dst) {
+    // Serialize version
+    const size_t capacity = JSON_OBJECT_SIZE(1);
+    DynamicJsonDocument doc(capacity);
+
+    // Create an object at the root
+    JsonObject root = doc.to<JsonObject>();
+
+    // Fill the object
+    thatVersion.save(root);
+
+    // Serialize JSON to file
+    return serializeJsonPretty(doc, dst) > 0;
+}
+
+bool deserializeVersion(const char * &src, ThatVersion &thatVersion) {
+    // Deserialize version
+    const size_t capacity = JSON_OBJECT_SIZE(1) + 50;
+    DynamicJsonDocument doc(capacity);
+
+    // Parse the JSON object in the file
+    DeserializationError err = deserializeJson(doc, src);
+    if (err) {
+        TV_NOT(F("no existing version"));
+        thatVersion.load(doc.as<JsonObject>());
+        TV_NOT(F("loaded default version"));
+        return true;
+    } else {
+        thatVersion.load(doc.as<JsonObject>());
+        TV_NOT(F("loaded existing version"));
+        return true;        
+    }
+    // TODO:  Can/should I return false here somehow?
+}
+
+void doPoll() {
+#ifdef TV_DEBUG
+    request.setDebug(true);
+#endif
+    request.onData(requestHandler);
+    sendRequest();
+}
+
+void ThatVersion::save(JsonObject obj) const {
+    obj["version"] = version;
+}
+
+void ThatVersion::load(JsonObjectConst obj) {
+    const char* v = obj["version"];
+    if (v)
+        strlcpy(version, v, sizeof(version));
+    else
+        strlcpy(version, "0.0.0", sizeof(version)); // Default
+}
--- src/thatVersion.h
+++ src/thatVersion.h
@@ -1,6 +1,6 @@
 /* Copyright (C) 2019-2020 Lee C. Bussy (@LBussy)
 
-This file is part of Lee Bussy's Brew Bubbbles (brew-bubbles).
+This file is part of Lee Bussy's Brew Bubbles (brew-bubbles).
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,39 +20,38 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _THATVERSION_H
+#define _THATVERSION_H
 
-#include "serial_setup.h"
+// #define TV_DEBUG // Enable Debug for module
+
 #include "config.h"
-#include "execota.h"
-#include "jsonconfig.h"
-#include "webserver.h"
-#include "wifi.h"
-#include "version.h"
-#include "pushtarget.h"
-#include "target.h"
-#include "bftarget.h"
-#include "pushhelper.h"
-#include "bubbles.h"
-#include "mdns.h"
-#include "ntp.h"
-#include "thatVersion.h"
-#include <DoubleResetDetect.h>
 #include <ArduinoLog.h>
+#include <ESPAsyncTCP.h>
+#include <asyncHTTPrequest.h>
+#include <ArduinoJson.h>
 #include <Arduino.h>
 
-// DRD_TIMEOUT =    Maximum number of seconds between resets that counts
-//                  as a double reset
-// DRD_ADDRESS =    Address to the block in the RTC user memory change it
-//                  if it collides with another usage of the address block
-//
-#define DRD_TIMEOUT 3.0
-#define DRD_ADDRESS 0x00
+struct ThatVersion {
+    char version[32] = {'0','.','0','.','0'};
+    
+    void load(JsonObjectConst);
+    void save(JsonObject) const;
+};
 
-extern struct Config config;
-extern bool loadConfig();
-extern const char *filename;
-extern struct ThatVersion thatVersion;
+void doPoll();
+void sendRequest();
+void requestHandler(void*, asyncHTTPrequest*, int);
+bool serializeVersion(const ThatVersion &, Print &);
+bool deserializeVersion(const char * &, ThatVersion &);
 
-#endif // _MAIN_H
+#ifdef TV_DEBUG
+    #include <ArduinoLog.h>
+    #define TV_NOT(...)   Log.notice(F("[TV Debug] in %s(): %s." CR), __func__, __VA_ARGS__);
+    #define TV_ERR(...)   Log.error(F("[TV Debug] in %s(): %s." CR), __func__, __VA_ARGS__);
+#else
+    #define TV_NOT(...)
+    #define TV_ERR(...)
+#endif // End control debug printing
+
+#endif // _THATVERSION_H
