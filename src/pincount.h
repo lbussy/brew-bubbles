d809644a323bfa062d95440ba40dc286a4562b8d
Lee Bussy <lee@bussy.org>
2020-04-04 10:31:12 -0500

    Move interrupts externally

---
+++ src/pincount.h
@@ -0,0 +1,32 @@
+/* Copyright (C) 2019-2020 Lee C. Bussy (@LBussy)
+
+This file is part of Lee Bussy's Brew Bubbbles (brew-bubbles).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+#ifndef _PINCOUNT_H
+#define _PINCOUNT_H
+
+#include "config.h"
+#include <Arduino.h>
+
+void interruptSetup();
+void ICACHE_RAM_ATTR handleInterrupt();
+
+#endif // _PINCOUNT_H
